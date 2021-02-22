206b1961f6c83c38f5a3b112e21b01fe1d049cc8
Nicola <kolya.vladimirsky@gmail.com>
2021-02-22 19:44:48 +0300

    added definitions to network manager

--- firstLabRPC/RPClibrary/NetworkManager.h
+++ firstLabRPC/RPClibrary/NetworkManager.h
@@ -4,12 +4,34 @@
 
 #pragma once
 
+struct manager_info
+{
+    std::string name;
+
+};
+
+enum PACKET
+{
+    HELLO,
+    REJECT,
+    FUNCTION,
+    MAX
+};
+
 enum class MANAGER_TYPE
 {
     SERVER,
     CLIENT
 };
 
+enum class MANAGER_MODE
+{
+    MANUAL,
+    FREQUENCY
+};
+/**
+ * @brief
+ */
 class NetworkManager
 {
 public:
@@ -17,8 +39,38 @@ public:
 
     virtual ~NetworkManager();
 
+    template<typename T>
+    void AddDataToPacket(T value)
+    {
 
+    }
+
+    void CreatePacket();
+
+    void SendPacket();
+
+    void ReceivePacket();
+
+    /** @brief Check received data per second */
+    void SetNetFrequency(float frequency);
+
+    inline void HaveReceivedData() const noexcept;
+
+    void SetManagerMode(MANAGER_MODE mode) const;
+
+    MANAGER_MODE GetManagerMode() const;
+
+    void Connect(const std::string& address);
+
+    void Tick(float deltaTime);
 
 protected:
 
+    bool bContainSendData = false;
+
+    bool bContainReceiveData = false;
+
+    float m_NetFrequency = 1.f;
+
+    MANAGER_MODE m_Mode = MANAGER_MODE::FREQUENCY;
 };
